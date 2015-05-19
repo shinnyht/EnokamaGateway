2f4f714f3391bc3cbb8c9dee3e284c87a01841f0
zukky <zukky@ht.sfc.keio.ac.jp>
2015-05-20 01:55:51 +0900

    zukky update Genova and Santander

---
+++ fundamental-sensor/EnoshimaGenovaData/SoxClient.js
@@ -0,0 +1,271 @@
+// server info (DO NOT EDIT)
+var boshService = "http://sox.ht.sfc.keio.ac.jp:5280/http-bind/";
+var xmppServer = "sox.ht.sfc.keio.ac.jp";
+var jid = "cloutfujisawa@sox.ht.sfc.keio.ac.jp";
+var password = "pAnAke!o";
+
+// (EDIT) Prepare varibles (but these cannot be used in processing.js)
+var EnoshimaSensorInfo = {};
+EnoshimaSensorInfo.weather = 0;
+EnoshimaSensorInfo.temperature = 0;
+EnoshimaSensorInfo.humidity = 0;
+EnoshimaSensorInfo.wind = 0;
+EnoshimaSensorInfo.weatherH = 0; 
+EnoshimaSensorInfo.temperatureH = 0;
+EnoshimaSensorInfo.humidityH =  0;
+EnoshimaSensorInfo.windH = 0;
+EnoshimaSensorInfo.luigi = 0;
+EnoshimaSensorInfo.word = "似ている！";
+EnoshimaSensorInfo.word2 = "genovaらしさを感じる"
+
+/*
+ * (EDIT) Prepare getter methods to call from processing.js
+ * To use javascript variables in processing.js
+ *  you have to call by getter methods
+ *
+ */
+
+function getEnoshimaGenova(){
+	return EnoshimaSensorInfo.luigi;
+}
+
+function getEnoshimaGenovaWord(){
+	return EnoshimaSensorInfo.word;
+}
+
+function getEnoshimaGenovaWord2(){
+	return EnoshimaSensorInfo.word2;
+}
+
+// takamasa edit
+function getEnoshimaWeather(){
+	if(parseInt(EnoshimaSensorInfo.weather) == 0){
+		return "晴れ";		
+	} else if(parseInt(EnoshimaSensorInfo.weather) == 1){
+		return "晴-曇";		
+	} else if(parseInt(EnoshimaSensorInfo.weather) == 2){
+		return "曇り";		
+	} else if(parseInt(EnoshimaSensorInfo.weather) == 3){
+		return "曇-雨";		
+	} else if(parseInt(EnoshimaSensorInfo.weather) == 4){
+		return "雨";		
+	} else {
+		return "雪";		
+	}
+}
+
+function getEnoshimaTemp(){
+	return parseInt(EnoshimaSensorInfo.temperature);		
+}
+
+function getEnoshimaHumi(){
+	return parseInt(EnoshimaSensorInfo.humidity);	
+}
+
+function getEnoshimaWind(){
+	return parseInt(EnoshimaSensorInfo.wind);
+}
+
+
+// takamasa edit
+function getGenovaWeather(){
+	if(parseInt(EnoshimaSensorInfo.weatherH) == 0){
+		return "晴れ";		
+	} else if(parseInt(EnoshimaSensorInfo.weatherH) == 1){
+		return "晴-曇";		
+	} else if(parseInt(EnoshimaSensorInfo.weatherH) == 2){
+		return "曇り";		
+	} else if(parseInt(EnoshimaSensorInfo.weatherH) == 3){
+		return "曇-雨";		
+	} else if(parseInt(EnoshimaSensorInfo.weatherH) == 4){
+		return "雨";		
+	} else {
+		return "雪";		
+	}
+
+}
+
+function getGenovaTemp(){
+	return parseInt(EnoshimaSensorInfo.temperatureH);		
+}
+
+function getGenovaHumi(){
+	return parseInt(EnoshimaSensorInfo.humidityH);		
+}
+
+function getGenovaWind(){
+	return parseInt(EnoshimaSensorInfo.windH);
+}
+/* 0: sunny, 1: cloudy, 2: rainy 3: sunny-cloudy 4:cloudy-rainy */
+function getWeather() {
+    return EnoshimaSensorInfo.weather;
+}
+
+// Called when cahnged sensor data
+// takamasa edit
+function SimilarityCalculation(){
+	var temp = parseFloat(EnoshimaSensorInfo.temperature);
+	var humi = parseFloat(EnoshimaSensorInfo.humidity);
+	var tempH = parseFloat(EnoshimaSensorInfo.temperatureH);
+	var humiH = parseFloat(EnoshimaSensorInfo.humidityH);
+	var weat = parseFloat(EnoshimaSensorInfo.weather);
+	var weatH = parseFloat(EnoshimaSensorInfo.weatherH);
+	var wind = parseFloat(EnoshimaSensorInfo.wind);
+	var windH = parseFloat(EnoshimaSensorInfo.windH);
+	var fukai = 0.81 * temp + 0.01 * humi * (0.99 * temp -14.3) + 46.3;
+	fukai = Math.floor(fukai * 10) /10;
+	var fukaiH = 0.81 * tempH + 0.01 * humiH * (0.99 * tempH -14.3) + 46.3;
+	fukaiH = Math.floor(fukaiH * 10) /10;
+	var luigi = 100 - 0.2 * Math.pow(fukai - fukaiH, 2) - 2 * Math.pow(weat -weatH, 2) - Math.pow(wind - windH, 2);
+	luigi = Math.floor(luigi * 10) / 10;
+	if(luigi >= 30){
+	EnoshimaSensorInfo.luigi = luigi;
+	}else{
+		EnoshimaSensorInfo.luigi = 30.0;
+		luigi = 30;	
+	}
+	if(luigi >= 80){
+		EnoshimaSensorInfo.word = "すごい似ている！"
+		EnoshimaSensorInfo.word2 = "気分はgenovaだね"
+	} else if(luigi >= 60){
+		EnoshimaSensorInfo.word = "似ている！"
+		EnoshimaSensorInfo.word2 = "genovaらしさを感じる"
+	} else if(luigi >= 40){
+		EnoshimaSensorInfo.word = "少し似ている！"
+		EnoshimaSensorInfo.word2 = "江の島時々genova"
+	} else if(luigi >= 20){
+		EnoshimaSensorInfo.word = "普通"
+		EnoshimaSensorInfo.word2 = "genovaを探そう"
+	} else {
+		EnoshimaSensorInfo.word = "残念"
+		EnoshimaSensorInfo.word2 = "genovaらしさは少ない"
+	}
+}
+
+// Called when received sensor data
+function eventListener(device, transducer) {
+    // (EDIT) check if the DEVICE name is the one you want
+    if(device=="江ノ島今日の天気"){
+        /*
+         * (EDIT) change below statements depending on
+         * which TRANSDUCER & what VALUE you want to use
+         */
+        if (transducer.id == "天気") {
+			var value = String(transducer.sensorData.rawValue);
+            if (value.indexOf("晴") > -1 && value.indexOf("曇") > -1) {
+                EnoshimaSensorInfo.weather = 1;
+            }
+            else if (value.indexOf("雨") > -1 && value.indexOf("曇") > -1) {
+                EnoshimaSensorInfo.weather = 3;
+            }
+            else if (value.indexOf("晴") > -1) {
+                EnoshimaSensorInfo.weather = 5;//0;
+            }
+            else if (value.indexOf("曇") > -1) {
+                EnoshimaSensorInfo.weather = 2;
+            }
+            else if (value.indexOf("雨") > -1) {
+                EnoshimaSensorInfo.weather = 4;
+            }
+            else {
+                EnoshimaSensorInfo.weather = 5;
+            }
+		}
+    }else if(device=="EnoshimaYachtHarbour"){
+		if (transducer.name == "気温"){
+			EnoshimaSensorInfo.temperature = transducer.sensorData.rawValue;
+		}
+		else if (transducer.id == "湿度"){
+			EnoshimaSensorInfo.humidity = transducer.sensorData.rawValue;
+		}
+		else if (transducer.id == "平均風速"){
+			EnoshimaSensorInfo.wind = transducer.sensorData.rawValue;
+		}
+	}else if(device=="genovaData"){
+		if (transducer.id == "weather") {
+			var valueH = String(transducer.sensorData.rawValue);
+			if (valueH.indexOf("晴") > -1 && valueH.indexOf("曇") > -1) {
+				EnoshimaSensorInfo.weatherH = 1;
+			}
+			else if (valueH.indexOf("雨") > -1 && valueH.indexOf("雲") > -1){
+				EnoshimaSensorInfo.weatherH = 3;
+			}
+			else if (valueH.indexOf("晴") > -1) {
+				EnoshimaSensorInfo.weatherH = 0;
+			}
+			else if (valueH.indexOf("雲") > -1) {
+				EnoshimaSensorInfo.weatherH = 2;
+			}
+			else if (valueH.indexOf("雨") > -1){
+				EnoshimaSensorInfo.weatherH = 4;
+			}
+			else {
+				EnoshimaSensorInfo.weatherH = 5;
+			}
+		}
+	}else if(device=="genova5"){
+		if (transducer.id == "temperature"){
+			EnoshimaSensorInfo.temperatureH = transducer.sensorData.rawValue;
+		} else if (transducer.id == "humidity"){
+			EnoshimaSensorInfo.humidityH = transducer.sensorData.rawValue;
+		} else if (transducer.id == "wind_speed"){
+			EnoshimaSensorInfo.windH = transducer.sensorData.rawValue;
+		}
+	}
+	SimilarityCalculation();
+}
+
+// Create new SoxClient when page is loaded
+$(document).ready(function() {
+    var client = new SoxClient(boshService, xmppServer, jid, password);
+    var soxEventListener = new SoxEventListener();
+    soxEventListener.connected = function(soxEvent) {
+        console.log("[SoxClient.js]" + soxEvent.soxClient);
+        status("Connected: " + soxEvent.soxClient);
+        client.unsubscribeAll();
+
+        var deviceNames = ["江ノ島今日の天気", "EnoshimaYachtHarbour", "genovadata", "genova5"];
+
+        if (!client.subscribeDevice()) {
+            status("[SoxClient.js] Counldn't subscribe device: " + soxEvent.soxClient);
+        }
+        deviceNames.forEach(function(name){
+            var device = new Device(name);
+            if (!client.subscribeDevice(device)) {
+                status("Couldn't send subscription request: "+device);
+            }           
+        });
+    };
+    soxEventListener.connectionFailed = function(soxEvent) {
+        status("Connection Failed: " + soxEvent.soxClient);
+    };
+    soxEventListener.subscribed = function(soxEvent) {
+        status("Subscribed: " + soxEvent.device);
+    };
+    soxEventListener.subscriptionFailed = function(soxEvent) {
+        status("Subscription Failed: " + soxEvent.device);
+    };
+    soxEventListener.metaDataReceived = function(soxEvent) {
+        status("Meta data received: " + soxEvent.device);
+    };
+    soxEventListener.sensorDataReceived = function(soxEvent) {
+        status("Sensor data received: " + soxEvent.device);
+        var transducers = soxEvent.device.transducers;
+
+        transducers.forEach(function(transducer) {
+            eventListener(soxEvent.device.name, transducer);
+        });
+    };
+
+    client.setSoxEventListener(soxEventListener);
+    client.connect();
+});
+
+function status(message) {
+    // var html = (new Date().toLocaleString() + " [SoxClient.js] " + message +
+    //     "<hr>\n" + $("#status").html());
+    // $("#status").html(html);
+    console.log("-------------------------------------");
+    console.log(message);
+    console.log("-------------------------------------");
+}
---
+++ fundamental-sensor/similarity-genova/genova.pde
@@ -0,0 +1,113 @@
+/* @pjs preload="../../img/map.png, ../../img/sunny.png, ../../img/cloudy.png, ../../img/rainy.png, ../../img/enone.png, ../../img/enonex.png, ../../img/wave.png, ../../img/bubble.png, ../../img/weatherbubble.png, ../../img/bubble3.png, ../../img/genova.png */
+
+int timer = 0;
+boolean small = true;
+boolean smallE = true;
+boolean textType = true;
+boolean weather1 = true;
+boolean left = true;
+String yashi = "../../img/genova.png";
+String bubble = "../../img/bubble3.png";
+
+// Processing default function (1)
+void setup() {
+	size(window.screen.width, window.screen.height);
+	background(0);
+	fill(0);
+	PFont fontA = loadFont("courier");
+	textFont(fontA, 20);  
+}
+
+// Processing default function (2)
+void draw(){
+	timer++;
+	drawBackground();
+	drawGenova();
+	drawBubble();
+	drawText();
+}
+
+void drawBackground() {
+	background(0);
+}
+
+void drawGenova() {
+	if (timer % 20 == 0) {
+		if (small) {
+			small = false;
+		} else {
+			small = true;	
+		}
+	}
+
+	String w = yashi;
+	if (small) {
+		drawImage(w, 0, 0, 500, 500);
+	} else {
+		drawImage(w, 0, 10, 500, 500);
+	}
+}
+
+void drawBubble(){
+	x = bubble;
+	drawImage(x, 550, 50, 500, 380);
+}
+
+void drawText(){
+	if(timer%100 == 0){
+		if(textType){
+			textType = false;
+		}else{
+			textType = true;
+		}
+	}
+	if(textType){
+		drawTextQ();
+	}else{
+		drawTextA();
+	}
+	drawNote();
+}
+
+
+// takamasa edit
+void drawTextQ(){
+	fill(0, 0, 0);
+	textSize(55);
+	text("今日の", 600, 150);
+	text("江の島とgenovaの", 600, 220);
+	text("似ている度は？", 600, 290);
+}
+
+void drawTextA(){
+	float luigi = getEnoshimaGenova();
+	String word = getEnoshimaGenovaWord();
+	String word2 = getEnoshimaGenovaWord2();
+	fill(255, 0, 0);
+	textSize(70);
+	text(luigi + "％", 600, 160);
+	fill(0, 0, 0);
+	textSize(45);
+	text(word, 590, 240);
+	text(word2, 590, 300);
+}
+
+
+// takamasa edit
+void drawNote(){
+	fill(0, 0, 0);
+	textSize(20);
+	text("「似ている度」は江の島とgenovaの本日の天候", 600, 340);
+	text("（天気、気温、湿度、風速）をもとに算出して", 600, 370);
+	text("おります", 600, 400);
+}
+
+void drawImage(String imgPath, x, y) {
+	PImage img = loadImage(imgPath);
+	image(img, x, y);
+}
+
+void drawImage(String imgPath, x, y, w, h) {
+	PImage img = loadImage(imgPath);
+	image(img, x, y, w, h);
+}
